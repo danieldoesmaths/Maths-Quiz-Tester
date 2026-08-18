76b7a4001eca5cf2535bbf3fd5d2520369330384
danieldoesmaths <daniel.chen.maths@gmail.com>
2026-08-18 20:17:34 +0100

    Draft 2: Added addition and multiplication

--- math_quiz.py
+++ math_quiz.py
@@ -7,27 +7,58 @@ while True:
     print("Welcome to the Maths Timetable quiz!")
 
     response = input("\nWould you like to start? (Type Yes or No): ")
+
     if response == "Yes":
         print("Alright, let's get started!")
-        for number in range(1, 11):
-            number_1 = randint(1, 12)
-            number_2 = randint(1, 12)
-            value = number_1 * number_2
-            answer = int(input(f"Question {number}: What is {number_1} x {number_2}? : "))
+        operation = int(input(
+            "Which operation would you like to be tested on?"
+            "\n\t1. Addition"
+            "\n\t2. Multiplication"
+            "\n"
+        ))
 
-            if answer == value:
-                print("Correct!")
-                score += 1
-            else:
-                print("Incorrect!")
+        if operation == 1:
+            for number in range(1, 11):
+                number_1 = randint(1, 12)
+                number_2 = randint(1, 12)
+                value = number_1 + number_2
+                answer = int(input(
+                    f"Question {number}: What is {number_1} + {number_2}? : "
+                ))
+                if answer == value:
+                    print("Correct!")
+                    score += 1
+                else:
+                    print("Incorrect!")
+            print(f"\nYou scored {score}/10")
+            break
 
-        print(f"\nYou scored {score}/10")
-        break
+        elif operation == 2:
+
+            for number in range(1, 11):
+                number_1 = randint(1, 12)
+                number_2 = randint(1, 12)
+                value = number_1 * number_2
+                answer = int(input(
+                    f"Question {number}: What is {number_1} x {number_2}? : "
+                ))
+
+                if answer == value:
+                    print("Correct!")
+                    score += 1
+
+                else:
+                    print("Incorrect!")
+            print(f"\nYou scored {score}/10")
+            break
+
+        else:
+            print("Incorrect response. Shutting down quiz...")
+            break
 
     elif response == "No":
         print("No worries, shutting down quiz...")
         break
-
+    
     else:
-
         print("That is not a response that I am looking for.\n\nRestarting...")
