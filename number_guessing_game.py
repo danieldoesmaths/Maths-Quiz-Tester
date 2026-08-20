f8d67d55cdacaf7453a1c73c47273667571c0f3e
danieldoesmaths <daniel.chen.maths@gmail.com>
2026-08-20 15:33:27 +0100

    Draft 2

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,13 +1,27 @@
 from random import randint
-random_generated_number = randint(0,100)
+
 print("Welcome to the number guessing game ")
-print("\nI have chosen a number between 0 to 100 can you guess it?")
+
+smallest_number = int(input("What is the smallest number that you want the range to be ?  :"))
+largest_number = int(input("What is the largest number that you want the range to be ?  :"))
+
+random_generated_number = randint(smallest_number,largest_number)
+
+print(f"\nAlright!,I have now chosen a number between {smallest_number} to {largest_number} can you guess it?")
+
+number_of_guesses = 0
+
 while True:
     guessed_number = int(input("Your chosen number  :"))
+    number_of_guesses += 1
     if guessed_number < random_generated_number:
+        print(f"\nGuess No.{number_of_guesses} : {guessed_number}")
         print("The number is higher!")
+        
     elif guessed_number > random_generated_number:
+        print(f"\nGuess No.{number_of_guesses} : {guessed_number}")
         print("The number is lower!" )
+        
     else:
         break
-print(f" You have guessed correctly ! , the number that I have is {random_generated_number}")
+print(f"\nFinal Stats\n\tThe Number:{random_generated_number}\n\tTotal Guesses:{number_of_guesses}")
