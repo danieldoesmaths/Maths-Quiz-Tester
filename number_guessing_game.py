3c46294cf8ea942ee513a8f97f139c05b34dd3d3
danieldoesmaths <daniel.chen.maths@gmail.com>
2026-08-20 16:06:46 +0100

    Draft 3

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,27 +1,47 @@
 from random import randint
 
-print("Welcome to the number guessing game ")
+def smallest_number():
+    smallest = int(input("What is the smallest number you want to be tested on? "))
+    return smallest
 
-smallest_number = int(input("What is the smallest number that you want the range to be ?  :"))
-largest_number = int(input("What is the largest number that you want the range to be ?  :"))
+def largest_number():
+    largest = int(input("What is the largest number you want to be tested on? "))
+    return largest
 
-random_generated_number = randint(smallest_number,largest_number)
+def check_range(smallest, largest):
+    if largest <= smallest:
+        return False
+    return True
 
-print(f"\nAlright!,I have now chosen a number between {smallest_number} to {largest_number} can you guess it?")
+print("Welcome to the number guessing game!")
+while True:
+    small = smallest_number()
+    large = largest_number()
+    if check_range(small, large):
+        break
+
+    print("Larger number needs to be greater than smaller number.")
+
+random_generated_number = randint(small, large)
+
+print(f"\nAlright! I have now chosen a number between {small} and {large}. Can you guess it?")
 
 number_of_guesses = 0
 
 while True:
-    guessed_number = int(input("Your chosen number  :"))
+    guessed_number = int(input("Your chosen number: "))
     number_of_guesses += 1
+    print(f"\nGuess No. {number_of_guesses}:{guessed_number}")
+
     if guessed_number < random_generated_number:
-        print(f"\nGuess No.{number_of_guesses} : {guessed_number}")
         print("The number is higher!")
-        
     elif guessed_number > random_generated_number:
-        print(f"\nGuess No.{number_of_guesses} : {guessed_number}")
-        print("The number is lower!" )
-        
+        print("The number is lower!")
     else:
+        print("Correct!")
         break
-print(f"\nFinal Stats\n\tThe Number:{random_generated_number}\n\tTotal Guesses:{number_of_guesses}")
+print(
+    f"\nFinal Stats"
+    f"\n\tThe Number: {random_generated_number}"
+    f"\n\tTotal Guesses: {number_of_guesses}"
+    )
